519a1c438281148f56ad15c1afe19b546a2686a0
root <ryan.claussen@gmail.com>
2018-04-02 23:17:16 -0500

    add transition timing to fade in

--- appdaemon/apps/brighten_lights.py
+++ appdaemon/apps/brighten_lights.py
@@ -26,20 +26,22 @@ class BrightenLights(hass.Hass):
   def motion(self, entity, attribute, old, new, kwargs):
     #TODO check if lamp is on.
     #TODO Reset lamp brightness to 1
-    if self.now_is_between(self.args["start_window"], self.args["end_window"]):
+    if self.now_is_between(self.args["start_window"], self.args["end_window"]) and new == 'on':
       if self.get_state(self.args["light"], attribute="brightness") == None:
         brightness = 1
         self.turn_on(self.args["light"], brightness = brightness)
       else:
         brightness = self.get_state(self.args["light"], attribute="brightness")
-
+      
       # Don't do anything if we are already at max brightness
       #if  int(float(self.get_state(self.args["brightness_slider"]))) == int(float(self.get_state(self.args["max_brightness_slider"]))):
       if int(float(brightness)) == 255:
         return
-      # If there is still movement in 20 seconds, then start bumping the brightness up
-      if new == 'on':
-        self.run_in(self.brighten, 20, entity_id = entity, last_increase = 0)
+
+      self.run_in(self.brighten, delay = self.args["transition_time_sec"], entity_id = entity, last_increase = 0)
+
+    else:
+      return
 
   # Increase the local brightness if the sensor is still on
   def brighten(self, kwargs):
@@ -52,7 +54,7 @@ class BrightenLights(hass.Hass):
         current_increase = (max_brightness - current_brightness) * 0.06
         new_brightness = current_brightness + current_increase
       else:
-        current_increase = int(float(kwargs["last_increase"]) * 1.5)
+        current_increase = int(float(kwargs["last_increase"]) * 1.1)
         new_brightness = current_brightness + current_increase
       # Make sure we are not going above the max brightness
       if new_brightness > max_brightness:
@@ -60,4 +62,4 @@ class BrightenLights(hass.Hass):
       self.log("Increasing brightness from {} to {}".format(current_brightness, new_brightness))
       self.turn_on(self.args["light"], brightness = new_brightness)
       # Check again in 20 seconds
-      self.run_in(self.brighten, 20, entity_id = kwargs["entity_id"], last_increase = current_increase)
+      self.run_in(self.brighten, delay = kwargs["delay"], entity_id = kwargs["entity_id"], last_increase = current_increase)
