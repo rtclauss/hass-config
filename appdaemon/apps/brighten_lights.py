c88244d35ac2baf44be39bb95bd2750a916fa451
rtclauss <ryan.claussen@gmail.com>
2018-04-12 11:23:28 -0500

    fix brightening of lights after resetting aqara motion timer back to default 60 seconds

--- appdaemon/apps/brighten_lights.py
+++ appdaemon/apps/brighten_lights.py
@@ -24,8 +24,9 @@ class BrightenLights(hass.Hass):
 
   # On motion brighten the lights in 20 seconds 
   def motion(self, entity, attribute, old, new, kwargs):
-    #TODO check if lamp is on.
-    #TODO Reset lamp brightness to 1
+    # Debug
+    self.log(', '.join(['{}={!r}'.format(k, v) for k, v in kwargs.items()]))
+    self.log("Detected Motion in {}".format(self.args["sensors"]))
     if self.now_is_between(self.args["start_window"], self.args["end_window"]) and new == 'on':
       if self.get_state(self.args["light"], attribute="brightness") == None:
         brightness = 1
@@ -46,23 +47,35 @@ class BrightenLights(hass.Hass):
   # Increase the local brightness if the sensor is still on
   def brighten(self, kwargs):
     # Debug
-    #self.log(', '.join(['{}={!r}'.format(k, v) for k, v in kwargs.items()]))
+    self.log(', '.join(['{}={!r}'.format(k, v) for k, v in kwargs.items()]))
 
     # If the motion sensor is still on, increase the brightness
     if self.get_state(kwargs["entity_id"]) == 'on':
-      current_brightness = int(float(self.get_state(self.args["light"], attribute="brightness")))
+      if self.get_state(self.args["light"], attribute="brightness") == None:
+        current_brightness = 1
+        self.turn_on(self.args["light"], brightness = brightness)
+      else:
+        current_brightness = self.get_state(self.args["light"], attribute="brightness")
       max_brightness = 255
       # Increase the brightness by 3% of the difference between current and max to start, then double that every time up to max
       if kwargs["last_increase"] == 0:
         current_increase = (max_brightness - current_brightness) * 0.06
+        if current_increase < 1:
+          current_increase = 1
         new_brightness = current_brightness + current_increase
       else:
         current_increase = int(float(kwargs["last_increase"]) * 1.1)
         new_brightness = current_brightness + current_increase
       # Make sure we are not going above the max brightness
+      at_max_brightness = False
       if new_brightness > max_brightness:
         new_brightness = int(max_brightness)
+        at_max_brightness = True
       self.log("Increasing brightness from {} to {}".format(current_brightness, new_brightness))
       self.turn_on(self.args["light"], brightness = new_brightness)
+      # check if we're done.
+      if at_max_brightness:
+        self.log("Done phasing in {}".format(self.args["light"]))
+        return
       # Check again in 20 seconds
       self.run_in(self.brighten, seconds = kwargs["delay"], delay = kwargs["delay"], entity_id = kwargs["entity_id"], last_increase = current_increase)
