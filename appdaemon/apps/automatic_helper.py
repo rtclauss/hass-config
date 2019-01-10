3f0f432deb0c07168aae40ce1ca1565abf667a38
rtclauss <ryan.claussen@gmail.com>
2019-01-10 10:29:22 -0600

    fix payload identification

--- appdaemon/apps/automatic_helper.py
+++ appdaemon/apps/automatic_helper.py
@@ -7,9 +7,9 @@ class AutomaticHelper(hass.Hass):
         self.listen_event(self.event_received, "automatic_update")
 
     def event_received(self, event_name, data, kwargs):
-        event_data = data["event"]
-        event_id = data["id"]
+        event_type = data["type"]
+        event_location = data["location"]
         event_received = datetime.now()
 
-        self.log("Automatic event received from {}. Event was: {}".format(event_id, event_data))
-        self.set_state("sensor.automatic_event", state = event_id, attributes = {"event_data": event_data, "event_received": str(event_received)})
+        self.log("Automatic event received from {}. Event was: {}".format(event_type, event_location))
+        self.set_state("sensor.automatic_event", state = event_type, attributes = {"event_data": event_location, "event_received": str(event_received)})
