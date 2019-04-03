d8165a034adcbc61ede7a9c6426938fd3771f432
rtclauss <ryan.claussen@gmail.com>
2019-04-03 13:09:29 -0500

    add more logic in case dark sky doesn't have a true temperature

--- appdaemon/apps/nest_travel_helper.py
+++ appdaemon/apps/nest_travel_helper.py
@@ -6,30 +6,41 @@ class NestTravelHelper(hass.Hass):
         self.phone = self.args['phone']
         self.thermostat = self.args["thermostat"]
         self.outside_temp_feels_like = self.args['outside_temp_feels_like']
+        self.outside_temp_is = self.args["outside_temp"]
         self.log("Configured to use {}, {}, and {} to determine nest conditions".format(self.driving_sensor, self.phone, self.thermostat, self.outside_temp_feels_like))
-        self.listen_state(self.state_changed, self.thermostat, attribute = "all")
-        
+        self.listen_state(self.state_changed, self.thermostat)
 
     def state_changed(self, entity, attribute, old, new, kwargs):
-        #self.log("Nest state for {} changed received from {}. Event was: {}".format(entity, old, new))
-        new_operation_mode = new['attributes']["operation_mode"]
-        old_operation_mode = old['attributes']["operation_mode"]
+        self.log("Nest state for {} changed from {} to {}".format(entity, old, new))
+        if new == old:
+            return
         #new_away_mode = new['attributes']["away_mode"]
         #old_away_mode = old['attributes']['away_mode']
         
         driving_state = self.get_state(self.driving_sensor)
         
-        outside_temp = float(self.get_state(self.outside_temp_feels_like))
-        if outside_temp < 45:
+        outside_temp_feels = self.get_state(self.outside_temp_feels_like)
+        outside_temp_is = self.get_state(self.outside_temp_is)
+        if outside_temp_feels == "unkown":
+            outside_temp = float(outside_temp_is)
+        elif outside_temp_is != "unknown":
+            outside_temp = float(outside_temp_feels)
+        else:
+            outside_temp = None
+
+        if outside_temp == None:
+            operation_mode = 'auto'
+        elif outside_temp < 50:
             operation_mode = 'heat'
-        elif 45 <= outside_temp < 67:
+        elif 50 <= outside_temp < 67:
             operation_mode = 'auto'
         elif outside_temp >= 67:
             operation_mode = 'cool'
         
         ## Not sure if I need to check if windows are open or not.  Nest should be turned off so no change in state should happen.
         
-        if driving_state == 'on' and new_operation_mode == "eco":
+        if driving_state == 'on' and new == "eco":
+            self.log("Calling set_operation_mode with values: {} {}".format(entity, operation_mode))
             self.call_service(self, 'climate/set_operation_mode', entity_id = entity, operation_mode = operation_mode)
         
         ## not sure if I need this next if statement
