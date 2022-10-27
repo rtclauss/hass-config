a796f3bb0eb5a727d35196ae3b70a8ae0467ea6b
rtclauss <ryan.claussen@gmail.com>
2022-10-27 13:10:53 -0500

    improve resolution and update speed thanks to more reliable sensors

--- appdaemon/apps/tracker.py
+++ appdaemon/apps/tracker.py
@@ -6,7 +6,8 @@ from datetime import datetime, timedelta, timezone
 from geopy.distance import great_circle
 
 from pygeodesy import ellipsoidalNvector
-from pygeodesy import ellipsoidalVincenty
+# from pygeodesy import ellipsoidalVincenty
+from pygeodesy import ellipsoidalKarney
 
 
 class BayesianDeviceTracker(hass.Hass):
@@ -47,8 +48,7 @@ class BayesianDeviceTracker(hass.Hass):
 
     def location_update(self, entity, attribute, old, new, kwargs):
         # self.log("in location_update")
-        self.log("triggered by: {} {} {} {} {}".format(
-            entity, attribute, old, new, kwargs))
+        # self.log("triggered by: {} {} {} {} {}".format(entity, attribute, old, new, kwargs))
         bayesian_state = self.get_state(self.bayesian, attribute="all")
         # self.log("here is the current bayesian tracker state: {}".format(bayesian_state))
 
@@ -137,7 +137,7 @@ class BayesianDeviceTracker(hass.Hass):
             else:
                 # Convert to mph
                 attributes['speed'] = (attributes['speed'] / 0.44704)
-                self.log("setting speed from ios to: %s".format(attributes['speed']))
+                self.log("setting speed from ios to: {}".format(attributes['speed']))
             self.log("new ios speed is: {}".format(
                 attributes['speed']))
         # elif 'speed' in attributes.keys():
@@ -153,134 +153,130 @@ class BayesianDeviceTracker(hass.Hass):
 
         if bayesian_state['state'] == "on":
             config = self.get_plugin_config()
-            #self.log(
-            #    "Bayesian sensor says I am home. Setting device_tracker to home")
-            #self.log("My current position is {}(Lat), {}(Long)".format(config["latitude"], config["longitude"]))
-            #self.log("here we go setting {} to home with GPS: Accuracy {}, Latitude: {}, Longitude: {}".format(self.bayesian_device_tracker_id, 0, config["latitude"], config["longitude"]))
+            self.log(
+               "Bayesian sensor says I am home. Setting device_tracker to home")
+            self.log("My current position is {}(Lat), {}(Long)".format(config["latitude"], config["longitude"]))
+            self.log("here we go setting {} to home with GPS: Accuracy {}, Latitude: {}, Longitude: {}".format(self.bayesian_device_tracker_id, 0, config["latitude"], config["longitude"]))
             self.call_service("device_tracker/see", dev_id=self.bayesian_device_tracker_id, attributes={
                               "course": 0.0, "speed": attributes['speed'], "home_probability": bayesian_state["attributes"]["probability"], "latitude": config["latitude"], "longitude": config["longitude"]},
                               gps=[config["latitude"], config["longitude"]])
         else:
-            #self.log("bayes says I am away")
+            self.log("bayes says I am away")
             if gps_attributes.keys() != {"latitude", "longitude", "gps_accuracy"}:
-                #self.log("{} {} {}".format(gps_attributes.get("tracker"), gps_attributes.get("motion"), type(gps_attributes.get("motion"))))
-                if gps_attributes.get("tracker") == "traccar" and gps_attributes.get("motion", False) == False:
-                    #self.log("Traccar not moving. Returning and using existing location data.")
-                    return
-                else:
+                # self.log("bayes away log: tracker {}, motion {}, type(motion) {}".format(gps_attributes.get("tracker"), gps_attributes.get("motion"), type(gps_attributes.get("motion"))))
+                try:
+                    self.log("My current position is {}(Lat), {}(Long)".format(gps_attributes["latitude"], gps_attributes["longitude"]))
+                    self.log("here we go setting {} to somewhere with GPS: Accuracy {}, Latitude: {}, Longitude: {}".format(self.bayesian_device_tracker_id, gps_attributes["gps_accuracy"], gps_attributes["latitude"], gps_attributes["longitude"]))
+                    attributes = copy.deepcopy(gps_attributes)
+                    # self.log("{}".format(attributes['battery']))
+                    probability = bayesian_state["attributes"]["probability"]
+                    attributes['home_probability'] = probability
+
+                    # We can get false positives, like WetHop flashing home on a geofence enter/exit.
+                    # Use the attributes of the bayesian sensor to determine if the transition is correct
+                    if probability <= bayesian_state["attributes"]["probability_threshold"] and sensor_state['state'] == "home":
+                        #self.error(
+                        #    "False positive jump to home zone.  Not updating Bayesian Sensor")
+                        #self.error(
+                        #    "Bayesian state: {}".format(bayesian_state))
+                        #self.error("Sensor state: {}".format(sensor_state))
+                        return
+
                     try:
-                        #self.log("My current position is {}(Lat), {}(Long)".format(gps_attributes["latitude"], gps_attributes["longitude"]))
-                        #self.log("here we go setting {} to somewhere with GPS: Accuracy {}, Latitude: {}, Longitude: {}".format(self.bayesian_device_tracker_id, gps_attributes["gps_accuracy"], gps_attributes["latitude"], gps_attributes["longitude"]))
-                        attributes = copy.deepcopy(gps_attributes)
-                        # self.log("{}".format(attributes['battery']))
-                        probability = bayesian_state["attributes"]["probability"]
-                        attributes['home_probability'] = probability
-
-                        # We can get false positives, like WetHop flashing home on a geofence enter/exit.
-                        # Use the attributes of the bayesian sensor to determine if the transition is correct
-                        if probability <= bayesian_state["attributes"]["probability_threshold"] and sensor_state['state'] == "home":
-                            #self.error(
-                            #    "False positive jump to home zone.  Not updating Bayesian Sensor")
-                            #self.error(
-                            #    "Bayesian state: {}".format(bayesian_state))
-                            #self.error("Sensor state: {}".format(sensor_state))
-                            return
-
-                        try:
-                            del attributes['battery']
-                        except KeyError as ke:
-                            #self.error("KeyError deleting {}: missing information from gps sensor. continuing...".format(ke))
-                            pass
-
-                        try:
-                            del attributes['battery_level']
-                        except KeyError as ke:
-                            #self.error("KeyError deleting {}: missing information from gps sensor. continuing...".format(ke))
-                            pass
-                        # Different trackers give the direction of travel in different ways, need a way to get the value!
-                        attributes['course'] = gps_attributes.get(
-                            'dirOfTravel', gps_attributes.get("course", gps_attributes.get('bearing', gps_attributes.get('heading', -1))))
-                        try:
-                            del attributes['dirOfTravel']
-                        except KeyError as ke:
-                            #self.error("KeyError deleting {}: missing information from gps sensor. continuing...".format(ke))
-                            pass
-                        try:
-                            del attributes['bearing']
-                        except KeyError as ke:
-                            #self.error("KeyError deleting {}: missing information from gps sensor. continuing...".format(ke))
-                            pass
-
-                        attributes['timestamp'] = gps_attributes.get(
-                            "timestamp", gps_attributes.get('last_changed', ""))
-                        try:
-                            del attributes['last_changed']
-                        except KeyError as ke:
-                            #self.error("KeyError deleting {}: missing information from gps sensor. continuing...".format(ke))
-                            pass
-                        try:
-                            del attributes['tracker']
-                        except KeyError as ke:
-                            #self.error("KeyError deleting {}: missing information from gps sensor. continuing...".format(ke))
-                            pass
-
-                        # To try and eliminate flashes backward in location from older/stale data, let's find the mean of the old location
-                        # and the new sensor data and use that mean as the location.
-                        dev_tracker_state = self.get_state(
-                            "device_tracker." + self.bayesian_device_tracker_id, attribute="all")
-                        # todo the old state may not have a latitude or longitude when hass restart
-                        #self.log("here is the previous device_tracker state: {}".format(dev_tracker_state))
-
-                        new_lat_log_p = ellipsoidalNvector.LatLon(lat=gps_attributes.get(
-                            "latitude"), lon=gps_attributes.get("longitude"))
-                        #self.log("new location is: {}".format(new_lat_log_p))
-
-                        try:
-                            old_lat_log_p = ellipsoidalNvector.LatLon(
-                                lat=dev_tracker_state["attributes"]["latitude"], lon=dev_tracker_state["attributes"]["longitude"])
-                        except KeyError as ke:
-                            # Home Assistant has restarted so we have no previous state for the device tracker. Let's use the new location state
-                            # for the old value.
-                            old_lat_log_p = new_lat_log_p
-                            #self.error("KeyError deleting {}: missing information from gps sensor. continuing...".format(ke))
-                            pass
-
-                        #self.log("old location is: {}".format(old_lat_log_p))
-
-                        # Let's try the intermediate calculation and change the mean depending on the speed.
-                        speed = attributes['speed']
-                        if speed > 35:
-                            update_ratio=0.7
-                        else:
-                            update_ratio=0.1
-                        mean_of_points = new_lat_log_p.intermediateTo(
-                            old_lat_log_p, update_ratio)
-                        #self.log("Mean of location between old and new is {}".format(
-                        #    mean_of_points))
-
-                        # self.log("{}".format(attributes))
-                        # For some reason setting attributes of gps coordinates overrides the gps data in device_tracker/see
-                        attributes['latitude'] = mean_of_points.lat
-                        attributes['longitude'] = mean_of_points.lon
-
-                        # Add source of update
-                        attributes['source_tracker'] = sensor_state['entity_id']
-
-                        # rtclauss add gps_update_time_attribute
-                        attributes['gps_updated'] = datetime.now(
-                            timezone.utc).isoformat()
-                        self.call_service("device_tracker/see", dev_id=self.bayesian_device_tracker_id,
-                                          attributes=attributes,
-                                          gps_accuracy=gps_attributes["gps_accuracy"],
-                                          # rtclauss 11/15/18 - use mean location
-                                          gps=[mean_of_points.lat,
-                                               mean_of_points.lon]
-                                          )
-                    except KeyError as e:
+                        del attributes['battery']
+                    except KeyError as ke:
+                        #self.error("KeyError deleting {}: missing information from gps sensor. continuing...".format(ke))
                         pass
-                        # self.error(
-                        #     "KeyError {}: missing information from sensor {}. Returning with no action.".format(e, sensor_state['entity_id']))
-                        #self.call_service("device_tracker/see", dev_id=self.bayesian_device_tracker_id, attributes={"home_probability": bayesian_state["attributes"]["probability"]})
+
+                    try:
+                        del attributes['battery_level']
+                    except KeyError as ke:
+                        #self.error("KeyError deleting {}: missing information from gps sensor. continuing...".format(ke))
+                        pass
+                    # Different trackers give the direction of travel in different ways, need a way to get the value!
+                    attributes['course'] = gps_attributes.get(
+                        'dirOfTravel', gps_attributes.get("course", gps_attributes.get('bearing', gps_attributes.get('heading', -1))))
+                    try:
+                        del attributes['dirOfTravel']
+                    except KeyError as ke:
+                        #self.error("KeyError deleting {}: missing information from gps sensor. continuing...".format(ke))
+                        pass
+                    try:
+                        del attributes['bearing']
+                    except KeyError as ke:
+                        #self.error("KeyError deleting {}: missing information from gps sensor. continuing...".format(ke))
+                        pass
+
+                    attributes['timestamp'] = gps_attributes.get(
+                        "timestamp", gps_attributes.get('last_changed', ""))
+                    try:
+                        del attributes['last_changed']
+                    except KeyError as ke:
+                        #self.error("KeyError deleting {}: missing information from gps sensor. continuing...".format(ke))
+                        pass
+                    try:
+                        del attributes['tracker']
+                    except KeyError as ke:
+                        #self.error("KeyError deleting {}: missing information from gps sensor. continuing...".format(ke))
+                        pass
+
+                    # To try and eliminate flashes backward in location from older/stale data, let's find the mean of the old location
+                    # and the new sensor data and use that mean as the location.
+                    dev_tracker_state = self.get_state(
+                        "device_tracker." + self.bayesian_device_tracker_id, attribute="all")
+                    # todo the old state may not have a latitude or longitude when hass restart
+                    #self.log("here is the previous device_tracker state: {}".format(dev_tracker_state))
+
+                    new_lat_log_p = ellipsoidalNvector.LatLon(lat=gps_attributes.get(
+                        "latitude"), lon=gps_attributes.get("longitude"))
+                    #self.log("new location is: {}".format(new_lat_log_p))
+
+                    try:
+                        old_lat_log_p = ellipsoidalNvector.LatLon(
+                            lat=dev_tracker_state["attributes"]["latitude"], lon=dev_tracker_state["attributes"]["longitude"])
+                    except KeyError as ke:
+                        # Home Assistant has restarted so we have no previous state for the device tracker. Let's use the new location state
+                        # for the old value.
+                        old_lat_log_p = new_lat_log_p
+                        #self.error("KeyError deleting {}: missing information from gps sensor. continuing...".format(ke))
+                        pass
+
+                    #self.log("old location is: {}".format(old_lat_log_p))
+                    
+                    # Let's try the intermediate calculation and change the mean depending on the speed.
+                    speed = attributes['speed']
+                    if speed > 10:
+                        update_ratio=0.1
+                    else:
+                        update_ratio=0.05
+                    mean_of_points = new_lat_log_p.intermediateTo(
+                        old_lat_log_p, update_ratio)
+                    #self.log("Mean of location between old and new is {}".format(
+                    #    mean_of_points))
+
+                    # self.log("{}".format(attributes))
+                    # For some reason setting attributes of gps coordinates overrides the gps data in device_tracker/see
+                    attributes['latitude'] = mean_of_points.lat
+                    attributes['longitude'] = mean_of_points.lon
+
+                    # Add source of update
+                    attributes['source_tracker'] = sensor_state['entity_id']
+
+                    # rtclauss add gps_update_time_attribute
+                    attributes['gps_updated'] = datetime.now(
+                        timezone.utc).isoformat()
+                    self.call_service("device_tracker/see", dev_id=self.bayesian_device_tracker_id,
+                                        attributes=attributes,
+                                        gps_accuracy=gps_attributes["gps_accuracy"],
+                                        # rtclauss 11/15/18 - use mean location
+                                        gps=[mean_of_points.lat,
+                                            mean_of_points.lon]
+                                        )
+                except KeyError as e:
+                    pass
+                    # self.error(
+                    #     "KeyError {}: missing information from sensor {}. Returning with no action.".format(e, sensor_state['entity_id']))
+                    #self.call_service("device_tracker/see", dev_id=self.bayesian_device_tracker_id, attributes={"home_probability": bayesian_state["attributes"]["probability"]})
             else:
                 self.error(
                     "missing information from gps sensor. Returning with no action.")
